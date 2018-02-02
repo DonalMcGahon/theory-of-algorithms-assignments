9d5218672185653062eb9abdc7ff26a824cf80e6
DonalMcGahon <dmcgahon1234@hotmail.com>
2018-02-02 17:33:30 +0000

    Added Prime number checker by brute force

--- Problem1/Problem1.rkt
+++ Problem1/Problem1.rkt
@@ -1,6 +1,10 @@
 #lang racket
 
-(display "Prime Number Checker")
+(display "Prime Number Checker\n")
 (define i 2) ; setting range as 2 because a prime number cannot be 1 or less
 
+(define (decide-prime? j) ; Creating a function called decide-prime
+(not (for/or [(a (in-range i j))] ; When we get a value back as #t for/or will return showing it is not a prime number
+((= 0 (remainder j a)))))) ; checking to see if j is dividable by a
 
+(decide-prime? 2);
