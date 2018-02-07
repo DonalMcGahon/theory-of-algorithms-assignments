def2a6a82dd2565af72ada9f560ef092a476a542
DonalMcGahon <dmcgahon1234@hotmail.com>
2018-02-07 22:29:55 +0000

    Problem2 Answer added

--- Probelm2/Problem2.rkt
+++ Probelm2/Problem2.rkt
@@ -1,1 +1,20 @@
 #lang racket
+; Adapted From - http://cs.umw.edu/~finlayson/class/spring15/cpsc401/samples/racket/collatz.rkt
+
+; return one step in the sequence
+(define (collatz n)
+  (if (even? n)
+    (/ n 2)
+    (+ 1 (* 3 n))))
+
+; recurse over all numbers
+(define (collatz-list n)
+  (if (= n 1)
+    (displayln "All done!")
+    (begin
+      (display "N = ")
+      (displayln n)
+      (collatz-list (collatz n)))))
+
+
+(collatz-list 9)
--- Probelm2/README.md
+++ Probelm2/README.md
@@ -1,1 +1,2 @@
 # Problem Sheet Question 2
+
