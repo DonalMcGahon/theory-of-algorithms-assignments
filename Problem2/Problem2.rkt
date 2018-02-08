3969ec8ef31959366786aa01351acd4e20d04e43
DonalMcGahon <dmcgahon1234@hotmail.com>
2018-02-08 21:07:25 +0000

    Added comments to code

--- Problem2/Problem2.rkt
+++ Problem2/Problem2.rkt
@@ -3,18 +3,22 @@
 
 ; return one step in the sequence
 (define (collatz n)
-  (if (even? n)
-    (/ n 2)
-    (+ 1 (* 3 n))))
+  (if (even? n); If number is even
+    (/ n 2); Divide it by 2
+    (+ 1 (* 3 n)))); Otherwise * it by 3 and then add 1
 
 ; recurse over all numbers
 (define (collatz-list n)
-  (if (= n 1)
-    (displayln "All done!")
+  (if (= n 1); If number equals 0
+    (displayln "Finished\n"); Finished
     (begin
       (display "N = ")
       (displayln n)
       (collatz-list (collatz n)))))
 
-
+(displayln "Number given is 5: ")
+(collatz-list 5)
+(displayln "Number given is 9: ")
 (collatz-list 9)
+(displayln "Number given is 2: ")
+(collatz-list 2)
