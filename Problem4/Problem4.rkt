c7e22b3a5a9a54ad57558a880484bfd1dc909162
DonalMcGahon <dmcgahon1234@hotmail.com>
2018-03-06 13:03:04 +0000

    Function added to Problem 4

--- Problem4/Problem4.rkt
+++ Problem4/Problem4.rkt
@@ -1,1 +1,10 @@
 #lang racket
+
+
+; Adapted from - https://stackoverflow.com/questions/41512010/sum-items-in-list-racket
+; Function to add all elements in a list together
+(define (my+ list)
+  (for/sum ((i list))
+    i))
+
+
