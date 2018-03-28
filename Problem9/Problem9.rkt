77e074073ca62e965a121d5001ff697a1f5699a2
DonalMcGahon <dmcgahon1234@hotmail.com>
2018-03-28 18:56:25 +0100

    Problem 9 racket started

--- Problem9/Problem9.rkt
+++ Problem9/Problem9.rkt
@@ -1,3 +1,10 @@
 #lang racket
 
 (display "Donal McGahon G00299627 Problem Sheet 9 \n \n")
+
+(define d null) ; define d as null
+(define (sod2 a b c) ; define chse with 3 elements a b & c
+  (if (null? a)(null? b)(null? c))
+      d ; if any of the lists are null return d
+      (if (not(= (length a)(length b)(length c)))
+          d d))  ; if any of the lists are not equal in length, return d
