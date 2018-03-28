85568433abd99ca31041d48dd94d16c15aed3ff0
DonalMcGahon <dmcgahon1234@hotmail.com>
2018-03-28 14:51:21 +0100

    Problem 6 racket complete

--- Problem6/Problem6.rkt
+++ Problem6/Problem6.rkt
@@ -1,3 +1,20 @@
 #lang racket
 
 (display "Donal McGahon G00299627 Problem Sheet 6 \n \n")
+
+(define (hamming-distance a b)
+(define c 0)
+; Defining a function with 2 lists and a int c with value 0.
+    (if (not (pair? a)) ; if a is not equal to 1
+        c ; return c, otherwise
+            (if (= (car a) (car b)) (hamming-distance (cdr a) (cdr b)) ; if a and b are equal, take the 2nd value from both lists
+            (+ 1 (hamming-distance (cdr a) (cdr b)))))); if they are not equal, add 1 to the distance
+
+
+(display "Hamming-Weight Tests: \n \n")
+(display "(list 1 0 1 0 1 1 1 0) (list 1 1 1 1 0 0 0 0) \n")
+(display "Answer = ")(hamming-distance (list 1 0 1 0 1 1 1 0) (list 1 1 1 1 0 0 0 0))
+(display "(list 1 0 1 0 0 0 0 0) (list 1 1 1 1 0 0 0 0) \n")
+(display "Answer = ")(hamming-distance (list 1 0 1 0 0 0 0 0) (list 1 1 1 1 0 0 0 0))
+(display "(list 1 0 1 1 1 1 1 0) (list 1 1 1 1 0 0 0 0) \n")
+(display "Answer = ")(hamming-distance (list 1 0 1 1 1 1 1 0) (list 1 1 1 1 0 0 0 0))
